30ee1e2336b5bc5b2da3f4c9910245358d6aa66d
Conor Morgan <conor@newlineasp.com>
2025-04-11 13:48:20 +0100

    Updated example to show list vs single object

--- Program.cs
+++ Program.cs
@@ -9,60 +9,95 @@ builder.Services.AddFusionCache()
 
 var app = builder.Build();
 
-app.MapGet("/test", async (IFusionCache cache) =>
+app.MapGet("/single", async (IFusionCache cache) =>
+{
+    var result = new Participant();
+    var user = new User
     {
-        var results = new List<Participant>();
-        var user = new User
-        {
-            ID = 100,
-            Name = "A User"
-        };
+        ID = 200,
+        Name = "Another User"
+    };
 
-        // Retrieve participants from cache
+    // Retrieve participant from cache
+    var cachedParticipant = await cache.TryGetAsync<Participant>(
+        $"participant:4",
+        options => { options.EnableAutoClone = true; });
+
+    if (!cachedParticipant.HasValue)
+    {
+        // Save participant to cache if missing
+        var participant = new Participant
+        {
+            ID = 1,
+            UserID = user.ID
+        };
+        await cache.SetAsync($"participant:4", participant, options => { options.EnableAutoClone = true; });
+    }
+    else result = cachedParticipant.Value;
+
+    // Set user object on cached participant
+    result.User = user;
+    /*
+     * The user object here is null, which shows Auto Clone is working as expected for objects not in a list.
+     */
+
+    return Results.Ok(result);
+});
+
+app.MapGet("/list", async (IFusionCache cache) =>
+{
+    var results = new List<Participant>();
+    var user = new User
+    {
+        ID = 100,
+        Name = "A User"
+    };
+
+    // Retrieve participants from cache
+    for (int i = 1; i <= 3; i++)
+    {
+        var cachedParticipant = await cache.TryGetAsync<Participant>(
+            $"participant:{i}",
+            options => { options.EnableAutoClone = true; });
+        if (cachedParticipant.HasValue)
+            results.Add(cachedParticipant.Value);
+    }
+
+    if (results.Count < 3)
+    {
+        // Generate 3 participants
+        var participants = new List<Participant>();
         for (int i = 1; i <= 3; i++)
         {
-            var cachedParticipant = await cache.TryGetAsync<Participant>(
-                $"participant:{i}",
+            participants.Add(new Participant
+            {
+                ID = i,
+                UserID = user.ID
+            });
+        }
+
+        // Store participants in cache
+        foreach (var participant in participants)
+        {
+            await cache.SetAsync($"participant:{participant.ID}", participant,
                 options => { options.EnableAutoClone = true; });
-            if (cachedParticipant.HasValue)
-                results.Add(cachedParticipant.Value);
         }
 
-        if (results.Count < 3)
-        {
-            // Generate 3 participants
-            var participants = new List<Participant>();
-            for (int i = 1; i <= 3; i++)
-            {
-                participants.Add(new Participant
-                {
-                    ID = i,
-                    UserID = user.ID
-                });
-            }
+        results.AddRange(participants);
+    }
 
-            // Store participants in cache
-            foreach (var participant in participants)
-            {
-                await cache.SetAsync($"participant:{participant.ID}", participant,
-                    options => { options.EnableAutoClone = true; });
-            }
-
-            results.AddRange(participants);
-        }
-
-        // Set user object on cached participants
-        foreach (var participant in results)
+    // Set user object on cached participants
+    foreach (var participant in results)
         /*
-         * `results` entries on the 2nd request here have `User` set on them despite Auto Clone being enabled &
-         * the original object being modified after SetAsync was called.
+         * `results` entries on the 2nd request here though have the `User` set on them despite Auto Clone
+         * being enabled.
          */
-        {
-            participant.User = user;
-        }
+    {
+        participant.User = user;
+    }
 
-        return Results.Ok(results);
-    });
+    return Results.Ok(results);
+});
 
 app.Run();
 
